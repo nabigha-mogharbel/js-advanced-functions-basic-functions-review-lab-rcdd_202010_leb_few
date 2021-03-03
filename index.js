2af3296cf7271a5a98714542d12d16d9c81baefe
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2021-03-03 20:33:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ mondayWork("work from home");
 
 function wrapAdjective(bb="*"){return function(dd="special"){return "You are " + bb+dd+bb+"!"}}
 
+<<<<<<< HEAD
 const Calculator= {add: function(a,b){return a+b}, subtract: function(a,b){return a-b}, multiply: function (a,b){return a*b}, divide: function(a,b){return a/b}}
 function actionApplyer(a,b){let startPoint=a
   for(let i=0; i<b.length; i++){startPoint=b[i](startPoint)}
   return startPoint
 }
+=======
+const Calculator= {function add(a,b){return a+b}, function subtract(a,b){return a-b}, function multiply(a,b){return a*b}}
+>>>>>>> 813a37c8400b2f7f51f3c7416da4d225ae0b9f2d
